f277ca5b7f50625f9f73b682d33506206bcb4d3f
Daoli <dli245@aucklanduni.ac.nz>
2026-08-14 17:41:56 +1200

    Set up database connection

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const usersRouter = require('./api/users/users');
+
+const app = express();
+
+app.use(express.json());
+app.use('/users', usersRouter);
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
