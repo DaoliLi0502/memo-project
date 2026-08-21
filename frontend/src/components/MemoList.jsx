bb1f8f26b61f7b1034f53070d3a057816124d3dc
Daoli <dli245@aucklanduni.ac.nz>
2026-08-21 13:54:11 +1200

    Add memo delete

--- frontend/src/components/MemoList.jsx
+++ frontend/src/components/MemoList.jsx
@@ -3,6 +3,21 @@ import { useEffect, useState } from "react"
 function MemoList({ newMemo }) {
     const [memos, setMemos] = useState([])
 
+    async function handleDelete(id) {
+        const response = await fetch(`http://localhost:3000/memos/${id}`, {
+            method: 'DELETE',
+            credentials: 'include'
+        })
+
+        console.log(response.status)
+
+        if (response.ok) {
+            setMemos((memos) => {
+                return memos.filter((memo) => {return memo.id !== id})
+            })
+        }
+    }
+
     useEffect(() => {
         async function getMemos() {
             const response = await fetch('http://localhost:3000/memos', {
@@ -36,6 +51,9 @@ function MemoList({ newMemo }) {
                         <p>Created: {memo.created_at}</p>
                         <p>Due: {memo.due_at}</p>
                         <p>Status: {memo.completed ? 'Completed' : 'Not completed'}</p>
+                        <p>Complete</p>
+                        <p>Edit</p>
+                        <p onClick={() => handleDelete(memo.id)}>Delete</p>
                     </div>
                 )
             })}
